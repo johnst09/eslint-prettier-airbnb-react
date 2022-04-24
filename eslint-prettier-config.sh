6aa76a805340dc19b288a61b0228efa853802ebb
johnst09 <spencer@sjohnston.com>
2022-04-24 11:59:22 -0500

    Update eslint-prettier-config.sh

--- eslint-prettier-config.sh
+++ eslint-prettier-config.sh
@@ -28,28 +28,6 @@ if [ -f ".eslintrc.js" -o -f ".eslintrc.yaml" -o -f ".eslintrc.yml" -o -f ".esli
 fi
 finished=false
 
-# Max Line Length Prompt
-while ! $finished; do
-  read -p "What max line length do you want to set for ESLint and Prettier? (Recommendation: 100)"
-  if [[ $REPLY =~ ^[0-9]{2,3}$ ]]; then
-    max_len_val=$REPLY
-    finished=true
-    echo
-  else
-    echo -e "${YELLOW}Please choose a max length of two or three digits, e.g. 80 or 100 or 120${NC}"
-  fi
-done
-
-# Trailing Commas Prompt
-echo "What style of trailing commas do you want to enforce with Prettier?"
-echo -e "${YELLOW}>>>>> See https://prettier.io/docs/en/options.html#trailing-commas for more details.${NC}"
-select trailing_comma_pref in "none" "es5" "all"; do
-  case $trailing_comma_pref in
-    "none" ) break;;
-    "es5" ) break;;
-    "all" ) break;;
-  esac
-done
 echo
 
 # ----------------------
@@ -61,12 +39,12 @@ echo -e "${GREEN}Configuring your development environment... ${NC}"
 echo
 echo -e "1/4 ${LCYAN}ESLint & Prettier Installation... ${NC}"
 echo
-npm install -D eslint prettier  eslint-plugin-react-hooks
+npm install -D eslint prettier eslint-plugin-react-hooks
 
 echo
 echo -e "2/4 ${YELLOW}Conforming to Airbnb's JavaScript Style Guide... ${NC}"
 echo
-npm install -D eslint-config-airbnb eslint-plugin-jsx-a11y eslint-plugin-import eslint-plugin-react babel-eslint
+npm install -D eslint-config-airbnb eslint-plugin-jsx-a11y eslint-plugin-import eslint-plugin-react
 
 echo
 echo -e "3/4 ${LCYAN}Making ESlint and Prettier play nice with each other... ${NC}"
@@ -83,122 +61,36 @@ else
 
   echo ${config_opening}'
   {
-  "extends": [
-    "airbnb",
-    "prettier",
-    "plugin:jsx-a11y/recommended",
-    "plugin:react-hooks/recommended",
-    "eslint:recommended", 
-    "plugin:react/recommended", 
-    "plugin:prettier/recommended"
-  ],
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "ecmaFeatures": {
-      "jsx": true,
-      "experimentalObjectRestSpread": true,
-      "impliedStrict": true,
-      "classes": true
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "browser": true,
+        "jest": true
     },
-    "sourceType": "module"
-  },
-  "env": {
-    "browser": true,
-    "node": true,
-    "es2021": true,
-    "jest": true
-  },
-  "rules": {
-    "react/react-in-jsx-scope": "off"
-    "react-hooks/rules-of-hooks": "error",
-    "no-debugger": 0,
-    "no-alert": 0,
-    "no-unused-vars": 1,
-    "prefer-const": [
-      "error",
-      {
-        "destructuring": "all"
-      }
+    "extends": [
+        "airbnb",
+        "prettier",
+        "plugin:jsx-a11y/recommended",
+        "plugin:react-hooks/recommended",
+        "eslint:recommended", 
+        "plugin:react/recommended", 
+        "plugin:prettier/recommended"
     ],
-    "arrow-body-style": [
-      2,
-      "as-needed"
-    ],
-    "no-unused-expressions": [
-      2,
-      {
-        "allowTaggedTemplates": true
-      }
-    ],
-    "no-param-reassign": [
-      2,
-      {
-        "props": false
-      }
-    ],
-    "no-console": 0,
-    "import/prefer-default-export": 1,
-    "import": 0,
-    "func-names": 0,
-    "space-before-function-paren": 0,
-    "comma-dangle": 0,
-    "max-len": 0,
-    "import/extensions": 0,
-    "no-underscore-dangle": 0,
-    "consistent-return": 0,
-    "react/display-name": 1,
-    "react/no-array-index-key": 0,
-    "react/react-in-jsx-scope": 0,
-    "react/prefer-stateless-function": 0,
-    "react/forbid-prop-types": 0,
-    "react/jsx-props-no-spreading": 0,
-    "react/no-unescaped-entities": 0,
-    "jsx-a11y/accessible-emoji": 0,
-    "react/require-default-props": 0,
-    "react/jsx-filename-extension": [
-      1,
-      {
-        "extensions": [
-          ".js",
-          ".jsx"
-        ]
-      }
-    ],
-    "radix": 0,
-    "no-shadow": "off",
-    "quotes": [
-      2,
-      "single",
-      {
-        "avoidEscape": true,
-        "allowTemplateLiterals": true
-      }
-    ],
-    "prettier/prettier": [
-      "error",
-      {
-        "trailingComma": "'${trailing_comma_pref}'",
-        "singleQuote": true,
-        "printWidth": '${max_len_val}'
-      }
-    ],
-    "jsx-a11y/href-no-hash": "off",
-    "jsx-a11y/anchor-is-valid": [
-      "warn",
-      {
-        "aspects": [
-          "invalidHref"
-        ]
-      }
-    ]
-  },
-  "plugins": [
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
     "prettier",
     "react",
     "react-hooks"
-  ]
-}' >> .eslintrc.json
+    ]
+    "rules": {
+    }
+  }' >> .eslintrc.json
 fi
 
 echo
